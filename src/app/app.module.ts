8bd3a67cc40afc7819cb5353bcdbbef60f13314f
DeepPlain <junmo.kang0818@gmail.com>
2017-08-09 02:42:43 +0900

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,60 +23,60 @@ import { TutorialPageModule } from '../pages/common/tutorial/tutorial.module';
 
 // company //
 import { CompanyTabsPageModule } from '../pages/company/company-tabs/company-tabs.module'
-  import { CompanyHomePageModule } from '../pages/company/company-home/company-home.module';
-    import { CompanyAlarmPageModule } from '../pages/company/company-alarm/company-alarm.module';
-    import { CompanyConfigurePageModule } from '../pages/company/company-configure/company-configure.module';
-  import { CompanyProjectPageModule } from '../pages/company/company-project/company-project.module';
-    import { CompanyProjectFeedbackPageModule } from '../pages/company/company-project-feedback/company-project-feedback.module';
-    import { CompanyProjectHomePageModule } from '../pages/company/company-project-home/company-project-home.module';
-    import { CompanyProjectLinkPageModule } from '../pages/company/company-project-link/company-project-link.module';
-    import { CompanyProjectPriceStatementPageModule } from '../pages/company/company-project-price-statement/company-project-price-statement.module';
-    import { CompanyProjectRegistrationPageModule } from '../pages/company/company-project-registration/company-project-registration.module';
-    import { CompanyProjectReportPageModule } from '../pages/company/company-project-report/company-project-report.module';
-    import { CompanyProjectSideMenuPageModule } from '../pages/company/company-project-side-menu/company-project-side-menu.module';
-    import { CompanyProjectStatsPageModule } from '../pages/company/company-project-stats/company-project-stats.module';
-    import { CompanyProjectStoryPageModule } from '../pages/company/company-project-story/company-project-story.module';
-    import { CompanyProjectStorySummaryPageModule } from '../pages/company/company-project-story-summary/company-project-story-summary.module';
-    import { CompanyProjectUserInfoPageModule } from '../pages/company/company-project-user-info/company-project-user-info.module';
-    import { CompanyProjectUserProfilePageModule } from '../pages/company/company-project-user-profile/company-project-user-profile.module';
-  import { CompanyNewsfeedPageModule } from '../pages/company/company-newsfeed/company-newsfeed.module';
-    import { CompanyNewsfeedStoryPageModule } from '../pages/company/company-newsfeed-story/company-newsfeed-story.module';
-  import { CompanyMypagePageModule } from '../pages/company/company-mypage/company-mypage.module';
-    import { CompanyAccountModificationFormPageModule } from '../pages/company/company-account-modification-form/company-account-modification-form.module';
-  import { CompanyInterviewPageModule } from '../pages/company/company-interview/company-interview.module';
-    import { CompanyProjectInterviewPageModule } from '../pages/company/company-project-interview/company-project-interview.module';
-    import { CompanyProjectInterviewWritingEditorPageModule } from '../pages/company/company-project-interview-writing-editor/company-project-interview-writing-editor.module';
+import { CompanyHomePageModule } from '../pages/company/company-home/company-home.module';
+  import { CompanyAlarmPageModule } from '../pages/company/company-alarm/company-alarm.module';
+  import { CompanyConfigurePageModule } from '../pages/company/company-configure/company-configure.module';
+import { CompanyProjectPageModule } from '../pages/company/company-project/company-project.module';
+  import { CompanyProjectFeedbackPageModule } from '../pages/company/company-project-feedback/company-project-feedback.module';
+  import { CompanyProjectHomePageModule } from '../pages/company/company-project-home/company-project-home.module';
+  import { CompanyProjectLinkPageModule } from '../pages/company/company-project-link/company-project-link.module';
+  import { CompanyProjectPriceStatementPageModule } from '../pages/company/company-project-price-statement/company-project-price-statement.module';
+  import { CompanyProjectRegistrationPageModule } from '../pages/company/company-project-registration/company-project-registration.module';
+  import { CompanyProjectReportPageModule } from '../pages/company/company-project-report/company-project-report.module';
+  import { CompanyProjectSideMenuPageModule } from '../pages/company/company-project-side-menu/company-project-side-menu.module';
+  import { CompanyProjectStatsPageModule } from '../pages/company/company-project-stats/company-project-stats.module';
+  import { CompanyProjectStoryPageModule } from '../pages/company/company-project-story/company-project-story.module';
+  import { CompanyProjectStorySummaryPageModule } from '../pages/company/company-project-story-summary/company-project-story-summary.module';
+  import { CompanyProjectUserInfoPageModule } from '../pages/company/company-project-user-info/company-project-user-info.module';
+  import { CompanyProjectUserProfilePageModule } from '../pages/company/company-project-user-profile/company-project-user-profile.module';
+import { CompanyNewsfeedPageModule } from '../pages/company/company-newsfeed/company-newsfeed.module';
+  import { CompanyNewsfeedStoryPageModule } from '../pages/company/company-newsfeed-story/company-newsfeed-story.module';
+import { CompanyMypagePageModule } from '../pages/company/company-mypage/company-mypage.module';
+  import { CompanyAccountModificationFormPageModule } from '../pages/company/company-account-modification-form/company-account-modification-form.module';
+import { CompanyInterviewPageModule } from '../pages/company/company-interview/company-interview.module';
+  import { CompanyProjectInterviewPageModule } from '../pages/company/company-project-interview/company-project-interview.module';
+  import { CompanyProjectInterviewWritingEditorPageModule } from '../pages/company/company-project-interview-writing-editor/company-project-interview-writing-editor.module';
 // company //
 
 // user //
 import { UserTabsPageModule } from '../pages/user/user-tabs/user-tabs.module'
-  import { UserHomePageModule } from '../pages/user/user-home/user-home.module';
-    import { UserAlarmPageModule } from '../pages/user/user-alarm/user-alarm.module';
-    import { UserConfigurePageModule } from '../pages/user/user-configure/user-configure.module';
-    import { UserPointExchangePageModule } from '../pages/user/user-point-exchange/user-point-exchange.module';
-    import { UserPointUsageHistoryPageModule } from '../pages/user/user-point-usage-history/user-point-usage-history.module';
-  import { UserProjectPageModule } from '../pages/user/user-project/user-project.module';
-    import { UserProjectFeedbackPageModule } from '../pages/user/user-project-feedback/user-project-feedback.module';
-    import { UserProjectFeedbackFormPageModule } from '../pages/user/user-project-feedback-form/user-project-feedback-form.module';
-    import { UserProjectFeedbackModificationEditorPageModule } from '../pages/user/user-project-feedback-modification-editor/user-project-feedback-modification-editor.module';
-    import { UserProjectFeedbackWritingEditorPageModule } from '../pages/user/user-project-feedback-writing-editor/user-project-feedback-writing-editor.module';
-    import { UserProjectHistoryPageModule } from '../pages/user/user-project-history/user-project-history.module';
-    import { UserProjectHomePageModule } from '../pages/user/user-project-home/user-project-home.module';
-    import { UserProjectLinkPageModule } from '../pages/user/user-project-link/user-project-link.module';
-    import { UserProjectOpinionWritingEditorPageModule } from '../pages/user/user-project-opinion-writing-editor/user-project-opinion-writing-editor.module';
-    import { UserProjectParticipationConditionFormPageModule } from '../pages/user/user-project-participation-condition-form/user-project-participation-condition-form.module';
-    import { UserProjectRewardFormPageModule } from '../pages/user/user-project-reward-form/user-project-reward-form.module';
-    import { UserProjectSideMenuPageModule } from '../pages/user/user-project-side-menu/user-project-side-menu.module';
-    import { UserProjectStoryPageModule } from '../pages/user/user-project-story/user-project-story.module';
-    import { UserProjectStorySummaryWritingEditorPageModule } from '../pages/user/user-project-story-summary-writing-editor/user-project-story-summary-writing-editor.module';
-  import { UserNewsfeedPageModule } from '../pages/user/user-newsfeed/user-newsfeed.module';
-    import { UserNewsfeedStoryPageModule } from '../pages/user/user-newsfeed-story/user-newsfeed-story.module';
-  import { UserMypagePageModule } from '../pages/user/user-mypage/user-mypage.module';
-    import { UserAccountModificationFormPageModule } from '../pages/user/user-account-modification-form/user-account-modification-form.module';
-    import { UserProfileModificationFormPageModule } from '../pages/user/user-profile-modification-form/user-profile-modification-form.module';
-  import { UserInterviewPageModule } from '../pages/user/user-interview/user-interview.module';
-    import { UserProjectInterviewPageModule } from '../pages/user/user-project-interview/user-project-interview.module';
-    import { UserProjectInterviewWritingEditorPageModule } from '../pages/user/user-project-interview-writing-editor/user-project-interview-writing-editor.module';
+import { UserHomePageModule } from '../pages/user/user-home/user-home.module';
+  import { UserAlarmPageModule } from '../pages/user/user-alarm/user-alarm.module';
+  import { UserConfigurePageModule } from '../pages/user/user-configure/user-configure.module';
+  import { UserPointExchangePageModule } from '../pages/user/user-point-exchange/user-point-exchange.module';
+  import { UserPointUsageHistoryPageModule } from '../pages/user/user-point-usage-history/user-point-usage-history.module';
+import { UserProjectPageModule } from '../pages/user/user-project/user-project.module';
+  import { UserProjectFeedbackPageModule } from '../pages/user/user-project-feedback/user-project-feedback.module';
+  import { UserProjectFeedbackFormPageModule } from '../pages/user/user-project-feedback-form/user-project-feedback-form.module';
+  import { UserProjectFeedbackModificationEditorPageModule } from '../pages/user/user-project-feedback-modification-editor/user-project-feedback-modification-editor.module';
+  import { UserProjectFeedbackWritingEditorPageModule } from '../pages/user/user-project-feedback-writing-editor/user-project-feedback-writing-editor.module';
+  import { UserProjectHistoryPageModule } from '../pages/user/user-project-history/user-project-history.module';
+  import { UserProjectHomePageModule } from '../pages/user/user-project-home/user-project-home.module';
+  import { UserProjectLinkPageModule } from '../pages/user/user-project-link/user-project-link.module';
+  import { UserProjectOpinionWritingEditorPageModule } from '../pages/user/user-project-opinion-writing-editor/user-project-opinion-writing-editor.module';
+  import { UserProjectParticipationConditionFormPageModule } from '../pages/user/user-project-participation-condition-form/user-project-participation-condition-form.module';
+  import { UserProjectRewardFormPageModule } from '../pages/user/user-project-reward-form/user-project-reward-form.module';
+  import { UserProjectSideMenuPageModule } from '../pages/user/user-project-side-menu/user-project-side-menu.module';
+  import { UserProjectStoryPageModule } from '../pages/user/user-project-story/user-project-story.module';
+  import { UserProjectStorySummaryWritingEditorPageModule } from '../pages/user/user-project-story-summary-writing-editor/user-project-story-summary-writing-editor.module';
+import { UserNewsfeedPageModule } from '../pages/user/user-newsfeed/user-newsfeed.module';
+  import { UserNewsfeedStoryPageModule } from '../pages/user/user-newsfeed-story/user-newsfeed-story.module';
+import { UserMypagePageModule } from '../pages/user/user-mypage/user-mypage.module';
+  import { UserAccountModificationFormPageModule } from '../pages/user/user-account-modification-form/user-account-modification-form.module';
+  import { UserProfileModificationFormPageModule } from '../pages/user/user-profile-modification-form/user-profile-modification-form.module';
+import { UserInterviewPageModule } from '../pages/user/user-interview/user-interview.module';
+  import { UserProjectInterviewPageModule } from '../pages/user/user-project-interview/user-project-interview.module';
+  import { UserProjectInterviewWritingEditorPageModule } from '../pages/user/user-project-interview-writing-editor/user-project-interview-writing-editor.module';
 // user //
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -102,58 +102,58 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TutorialPageModule,
 
     CompanyTabsPageModule,
-      CompanyHomePageModule,
-        CompanyAlarmPageModule,
-        CompanyConfigurePageModule,
-      CompanyProjectPageModule,
-        CompanyProjectFeedbackPageModule,
-        CompanyProjectHomePageModule,
-        CompanyProjectLinkPageModule,
-        CompanyProjectPriceStatementPageModule,
-        CompanyProjectRegistrationPageModule,
-        CompanyProjectReportPageModule,
-        CompanyProjectSideMenuPageModule,
-        CompanyProjectStatsPageModule,
-        CompanyProjectStoryPageModule,
-        CompanyProjectStorySummaryPageModule,
-        CompanyProjectUserInfoPageModule,
-        CompanyProjectUserProfilePageModule,
-      CompanyNewsfeedPageModule,
-        CompanyNewsfeedStoryPageModule,
-      CompanyMypagePageModule,
-        CompanyAccountModificationFormPageModule,
-      CompanyInterviewPageModule,
-        CompanyProjectInterviewPageModule,
-        CompanyProjectInterviewWritingEditorPageModule,
+    CompanyHomePageModule,
+      CompanyAlarmPageModule,
+      CompanyConfigurePageModule,
+    CompanyProjectPageModule,
+      CompanyProjectFeedbackPageModule,
+      CompanyProjectHomePageModule,
+      CompanyProjectLinkPageModule,
+      CompanyProjectPriceStatementPageModule,
+      CompanyProjectRegistrationPageModule,
+      CompanyProjectReportPageModule,
+      CompanyProjectSideMenuPageModule,
+      CompanyProjectStatsPageModule,
+      CompanyProjectStoryPageModule,
+      CompanyProjectStorySummaryPageModule,
+      CompanyProjectUserInfoPageModule,
+      CompanyProjectUserProfilePageModule,
+    CompanyNewsfeedPageModule,
+      CompanyNewsfeedStoryPageModule,
+    CompanyMypagePageModule,
+      CompanyAccountModificationFormPageModule,
+    CompanyInterviewPageModule,
+      CompanyProjectInterviewPageModule,
+      CompanyProjectInterviewWritingEditorPageModule,
 
     UserTabsPageModule,
-      UserHomePageModule,
-        UserAlarmPageModule,
-        UserConfigurePageModule,
-        UserPointExchangePageModule,
-        UserPointUsageHistoryPageModule,
-      UserProjectPageModule,
-        UserProjectFeedbackPageModule,
-        UserProjectFeedbackFormPageModule,
-        UserProjectFeedbackModificationEditorPageModule,
-        UserProjectFeedbackWritingEditorPageModule,
-        UserProjectHistoryPageModule,
-        UserProjectHomePageModule,
-        UserProjectLinkPageModule,
-        UserProjectOpinionWritingEditorPageModule,
-        UserProjectParticipationConditionFormPageModule,
-        UserProjectRewardFormPageModule,
-        UserProjectSideMenuPageModule,
-        UserProjectStoryPageModule,
-        UserProjectStorySummaryWritingEditorPageModule,
-      UserNewsfeedPageModule,
-        UserNewsfeedStoryPageModule,
-      UserMypagePageModule,
-        UserAccountModificationFormPageModule,
-        UserProfileModificationFormPageModule,
-      UserInterviewPageModule,
-        UserProjectInterviewPageModule,
-        UserProjectInterviewWritingEditorPageModule,
+    UserHomePageModule,
+      UserAlarmPageModule,
+      UserConfigurePageModule,
+      UserPointExchangePageModule,
+      UserPointUsageHistoryPageModule,
+    UserProjectPageModule,
+      UserProjectFeedbackPageModule,
+      UserProjectFeedbackFormPageModule,
+      UserProjectFeedbackModificationEditorPageModule,
+      UserProjectFeedbackWritingEditorPageModule,
+      UserProjectHistoryPageModule,
+      UserProjectHomePageModule,
+      UserProjectLinkPageModule,
+      UserProjectOpinionWritingEditorPageModule,
+      UserProjectParticipationConditionFormPageModule,
+      UserProjectRewardFormPageModule,
+      UserProjectSideMenuPageModule,
+      UserProjectStoryPageModule,
+      UserProjectStorySummaryWritingEditorPageModule,
+    UserNewsfeedPageModule,
+      UserNewsfeedStoryPageModule,
+    UserMypagePageModule,
+      UserAccountModificationFormPageModule,
+      UserProfileModificationFormPageModule,
+    UserInterviewPageModule,
+      UserProjectInterviewPageModule,
+      UserProjectInterviewWritingEditorPageModule,
 
     BrowserModule,
     IonicModule.forRoot(MyApp)
