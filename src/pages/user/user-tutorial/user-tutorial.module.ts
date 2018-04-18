ba1a205b6df97e0b086d2d8833d7057d4783d129
AugustMD <sungjin.park0129@gmail.com>
2018-04-18 22:16:51 +0900

    유저 튜토리얼 진행중 1

--- src/pages/user/user-tutorial/user-tutorial.module.ts
+++ src/pages/user/user-tutorial/user-tutorial.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UserTutorialPage } from './user-tutorial';
+import { PipesModule } from '../../../pipes/pipes.module';
+import { MomentModule } from 'angular2-moment';
+import { ComponentsModule } from './../../../assets/components/components.module';
+import { ChartsModule } from 'ng2-charts';
+import 'chart.js';
+import { ElasticModule } from 'angular2-elastic';
+import { Ionic2RatingModule } from 'ionic2-rating';
 
 @NgModule({
   declarations: [
     UserTutorialPage,
   ],
   imports: [
+    ComponentsModule,
+    Ionic2RatingModule,
+    PipesModule,
+    MomentModule,
+    ElasticModule,
+    ChartsModule,
     IonicPageModule.forChild(UserTutorialPage),
   ],
 })
