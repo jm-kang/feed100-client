a00d3fbe52004f38b6af8f29031ad463d2dc47b4
AugustMD <sungjin.park0129@gmail.com>
2018-05-03 16:42:07 +0900

    tappable 추가 및 유저 튜토리얼 진행중

--- src/pages/user/user-tutorial/user-tutorial.ts
+++ src/pages/user/user-tutorial/user-tutorial.ts
@@ -1,4 +1,4 @@
-import { ModalWrapperPage } from './../../common/modal-wrapper/modal-wrapper';
+// import { ModalWrapperPage } from './../../common/modal-wrapper/modal-wrapper';
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, Slides, Content, Platform, AlertController, ActionSheetController } from 'ionic-angular';
 import { ThemeableBrowser, ThemeableBrowserOptions, ThemeableBrowserObject } from '@ionic-native/themeable-browser';
@@ -24,7 +24,20 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: 'user-tutorial.html',
 })
 export class UserTutorialPage {
+  isInfoHide = true;
   openTutorialIntro;
+  openProjectTabs;
+  openProjectTabsIntro;
+
+  // 추천 프로젝트
+  recommendedProjects = [
+    {
+      project_id: 1,
+      project_main_image:"./../../assets/img/project-main-image5.png",
+      project_name: "하플리 : 예쁜 한복들만 모았다! 허리치마, 저고리, 한복원피스 등.",
+      project_max_reward: 5500,
+    },
+  ];
   
   constructor(
     public navCtrl: NavController,
@@ -35,17 +48,33 @@ export class UserTutorialPage {
     private domSanitizer: DomSanitizer,
     private photoViewer: PhotoViewer,
     public actionSheetCtrl: ActionSheetController,
-    public keyboard: Keyboard,
-    public ModalWrapperPage: ModalWrapperPage) {
+    public keyboard: Keyboard) {
   }
 
   ionViewDidLoad(){
     console.log('ionViewDidLoad UserTutorialPage');
   }
 
+  
   ionViewWillEnter() {
     console.log('ionViewDidLoad UserTutorialPage');
+    this.isInfoHide = true;
     this.openTutorialIntro = true;
+    this.openProjectTabs = false;
+    this.openProjectTabsIntro = false;
+  }
+  
+  accessProjectCard(project_id) {
+    this.isInfoHide = true;
+    this.openTutorialIntro = false;
+    this.openProjectTabs = false;
+    this.openProjectTabsIntro = true;
   }
 
+  openProjectTabsPage() {
+    this.isInfoHide = false;
+    this.openTutorialIntro = false;
+    this.openProjectTabs = true;
+    this.openProjectTabsIntro = false;
+  }
 }
